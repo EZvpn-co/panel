2191e214f6be62b686987da61aca06db41c0b0d8
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-27 23:37:44 +0330

    app profile no rules

--- config/appprofile.php
+++ config/appprofile.php
@@ -4,9 +4,33 @@ declare(strict_types=1);
 
 $_ENV['Clash_Config'] = [
     'proxy-groups' => [
+        // [
+        //     'name' => '🔰 Type',
+        //     'type' => 'select',
+        //     'proxies' => [],
+        // ],
         [
-            'name' => '🔰 Type',
+            'name' => '🔰 节点选择',
             'type' => 'select',
+            // 插入节点名称
+            'proxies' => [
+                '♻️ 自动选择',
+                '🎯 全球直连',
+            ],
+        ],
+        [
+            'name' => '🎯 全球直连',
+            'type' => 'select',
+            'proxies' => [
+                'DIRECT',
+            ],
+        ],
+        [
+            'name' => '♻️ 自动选择',
+            'type' => 'url-test',
+            'url' => 'http://www.gstatic.com/generate_204',
+            'interval' => 300,
+            // 插入节点名称
             'proxies' => [],
         ],
         // [
